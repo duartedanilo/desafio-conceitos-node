fa2decf570745da8571babef5d15f0caded14809
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-06-29 20:45:49 -0300

    feat: put endpoint

--- src/app.js
+++ src/app.js
@@ -26,7 +26,22 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+	const { id } = request.params;
+	const { title, url, techs } = request.body;
+
+	const repositoryIndex = repositories.findIndex(
+		(repository) => repository.id === id
+	);
+
+	if (repositoryIndex < 0) {
+		return response.status(400).json({ error: "ID not found" });
+	}
+
+	const repository = { ...repositories[repositoryIndex], title, url, techs };
+
+	repositories[repositoryIndex] = repository;
+
+	return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
