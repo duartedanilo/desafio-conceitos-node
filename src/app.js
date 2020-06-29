4ec887394bcf8453911c6a77fc16df6b4d589374
Danilo Duarte <danilo.duarte8@hotmail.com>
2020-06-29 20:45:23 -0300

    feat: post endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -15,7 +16,13 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+	const { title, url, techs } = request.body;
+
+	const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+	repositories.push(repository);
+
+	return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
